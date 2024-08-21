ce68a9e351f65ac625df2ba0988412fa10637dc2
rockyrapanan <116909659+rockyrapanan@users.noreply.github.com>
2024-08-20 20:33:00 -0700

    Update functionScope.js

--- functionScope.js
+++ functionScope.js
@@ -43,3 +43,70 @@ function rollDice(){
     console.log(roll)
 }
 callTwice(rollDice);
+// Return Functions
+function MakeMysteryFunc(){
+    const rand = Math.random();
+    if (rand > 0.5){
+        return function(){
+            console.log("Congrats yopu got the function right")
+            console.log("It looks great!")
+        }
+    } else{
+        return function(){
+            alert("You got a virus in your computer!")
+            alert("Good luck trying to fix your computer lol!")
+        }
+    }
+}
+// Functions between two numbers.
+function isBetween(num){
+    return num >= 50 && num <= 100
+}
+
+// This function will create a factory function.
+/*
+ if  you create const function with min and max variables is will
+ store those numbers in function. 
+ Example: const function child(0, 15)
+          const function adult(18,50)
+
+*/
+function makeBetweenFunc(min, max){
+    return function(num){
+        return num >= min && num <= max;
+    }
+}
+
+// Math function (METHODS)
+
+const myMath = {
+    PI: 3.14159,
+    square (num){
+        return num * num;
+    },
+    cube (num) {
+        return num  ** 3;
+    },
+   
+}
+// Coding Exercise:
+/*
+Define an object called square, which will hold methods that have to do with the geometry of squares. It should contain two methods, 
+  area and perimeter area should accept the length of a side (all sides are the same in a square) 
+  and then return the side squared. perimeter should accept the length of a side and return 
+  that side multiplied by 4. 
+
+    square.area(10) //100
+    square.perimeter(10) //40
+
+
+*/
+//Answer:
+const square = {
+    area(num){
+        return num * num;
+    },
+    perimeter(num){
+        return num + num + num + num;
+    },
+}
